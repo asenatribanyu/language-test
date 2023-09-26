6aaedaf772b9bc9d270add8b8688c77dbb2bd0d9
zaldyalfarez <zaldyalfarez@gmail.com>
2023-09-26 11:15:04 +0700

    Login View Fix

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="flex justify-center lg:mt-7">
+        <div class="text-center text-gray-500 dark:text-white">
+            <div>
+                &copy; {{ date('Y') }} Lembaga Bahasa | Widyatama University
+            </div>
+        </div>
+    </div>
+</footer>
