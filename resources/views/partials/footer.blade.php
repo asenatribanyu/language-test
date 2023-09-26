6053e3b05f431e5b5308e1e56b74e659cd5d921e
zaldyalfarez <zaldyalfarez@gmail.com>
2023-09-26 11:54:56 +0700

    Forgot Password View Fix

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="flex justify-center lg:mt-7">
+        <div class="text-center text-gray-500 dark:text-white">
+            <div>
+                &copy; {{ date('Y') }} Language Institute | Widyatama University
+            </div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/forgot-password', function () {
-    return view('forgot-password');
+    return view('pages/forgot-password');
 });
