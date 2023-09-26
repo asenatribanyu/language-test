77c7aeb893533634e4a7756b343587a209b1de74
zaldyalfarez <zaldyalfarez@gmail.com>
2023-09-26 12:38:35 +0700

    Web Route Fix, and href

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages/login');
-  
+});
+
 Route::get('/forgot-password', function () {
     return view('pages/forgot-password');
+});
