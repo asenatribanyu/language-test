7806c4666bd5568866fb464a432cd533ec0e1574
Bilal Restu Mutaqien <bmutaqien@gmail.com>
2023-09-26 12:59:36 +0700

    membuat register view fix

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages/login');
 });
+
+Route::get('/register', function () {
+    return view('pages/register');
+});
