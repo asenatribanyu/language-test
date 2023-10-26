fce4d04b11ffd2dcd0b02b55c8d2c2c1c2ec9003
Bilal Restu Mutaqien <bmutaqien@gmail.com>
2023-10-26 13:30:19 +0700

    merapihkan page profil

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -12,7 +12,7 @@
 <div class="max-w-screen-xl my-2as border border-slate-200 rounded-xl p-10 mx-28 min-h-screen shadow-md">
 <h1 class="font-bold" > My Profile</h1>
     <hr class="h-px my-8 bg-gray-200 border-2 dark:bg-gray-700">
-        <div class=" max-w-lg border-slate-200 rounded-xl mx-auto shadow-md p-5">
+        <div class=" w-full border-slate-200 rounded-xl shadow-md p-5">
             <form action="">
             <label for="crete">
             <span class="block font-semibold mb-1 text-slate-700 ">Created</span>
@@ -49,11 +49,12 @@
             <input type="create" id="nama" placeholder="Coba 1" class="px-3 py-2 border shadow rounded w-full block text-sm">
                 </label>   
             </form>                                                             
-            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload Picture</label>
+            <label class="block mb-2 text-sm font-bold text-gray-900 dark:text-white" for="file_input">Upload Picture</label>
             <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
                 </div>
     <hr class="h-px my-8 bg-gray-200 border-2 dark:bg-gray-700">
-    <button class="my-10 bg-sky-500 px-5 py-2 rounded-full text-white font-semibold block mx-auto hover:bg-sky-600">Save</button>
+    <button class="my-10 bg-sky-500 px-5 ml-9 py-2 rounded-full text-white font-semibold block mx-auto hover:bg-sky-600">Save</button>
     </div>
+    @include('partials.footer')
 </body>
 </html>
