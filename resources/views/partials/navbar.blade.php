b6e6766d2df03f8662830e670437ba0762960d09
Ilham  Maruf <ilham.maruf@widyatama.ac.id>
2023-12-05 14:20:41 +0700

    perubahan fitur3

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -37,6 +37,11 @@
           </li>
           <li>
             <a href="/admin/dashboard/manage-exam" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Manage Exam</a>
+              <div class="sub-menu-2">
+                  <ul>
+                      <li></li>
+                  </ul>
+              </div>
           </li>
           <li>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Manage Practice</a>
