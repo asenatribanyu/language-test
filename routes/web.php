23994e0eb2278786531701f15c7f95eede6766d0
Ilham  Maruf <ilham.maruf@widyatama.ac.id>
2023-12-05 14:28:46 +0700

    tes ept1

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,6 @@ Route::get('/admin/dashboard/manage-users', function () {
     return view('/pages/manage-users');
 });
 
-Route::get('/admin/dashboard/manage-exam', function () {
+Route::get('/admin/dashboard/toeic-manage-question', function () {
     return view('/pages/toeic-manage-question');
 });
