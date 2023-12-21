125553fb4180fc30f8a95c952cfa2a61a05a97bd
zaldyalfarez <zaldyalfarez@gmail.com>
2023-12-21 12:57:35 +0700

    Ganti warna heading Exam Rules

--- resources/views/pages/waitingAreaEnroll.blade.php
+++ resources/views/pages/waitingAreaEnroll.blade.php
@@ -86,7 +86,7 @@
                         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
                         data-accordion-target="#accordion-open-body-1" aria-expanded="false"
                         aria-controls="accordion-open-body-1">
-                        <span class="flex text-blue-600 dark:text-blue-500 items-center"><svg class="w-5 h-5 me-2 shrink-0"
+                        <span class="flex text-red-600 dark:text-red-500 items-center"><svg class="w-5 h-5 me-2 shrink-0"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
