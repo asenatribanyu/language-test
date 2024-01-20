d07722b319e4a38707ec0fbfa1053efb58aa5ee9
zaldyalfarez <zaldyalfarez@gmail.com>
2024-01-20 15:52:32 +0700

    Back and Cancel href Navigation

--- resources/views/admin/exam/toeic/uploadStory.blade.php
+++ resources/views/admin/exam/toeic/uploadStory.blade.php
@@ -19,8 +19,8 @@
                 <div class="flex mt-5 items-center rounded-b dark:border-gray-600">
                     <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
-                    <button type="button"
-                        class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Back</button>
+                    <a href="{{ url('/admin/dashboard/exam/' . session('id')) . '/edit' }}"
+                        class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Back</a>
                 </div>
                 <div id="popup-modal" tabindex="-1"
                     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
